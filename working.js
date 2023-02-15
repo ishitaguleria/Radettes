4630bf74978ef5b37e7c8f16ed2768b9f709094e
ishitaguleria <106906746+ishitaguleria@users.noreply.github.com>
2023-02-16 02:10:44 +0530

    Add files via upload

--- working.js
+++ working.js
@@ -1,6 +1,12 @@
 let changeIcon = function(icon){
     icon.classList.toggle('fa-circle-pause')
 }
-let changeColor = function(icon){
-    icon.classList.toggle('blue')
+let colorChange = function(icon){
+    icon.classList.toggle('fa-repeat-1')
 }
+var myModal = document.getElementById('containerpopup')
+var myInput = document.getElementById('popup')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
